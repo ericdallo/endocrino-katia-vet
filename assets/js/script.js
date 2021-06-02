f08378a98b5efc7bba0d21b4e59b166ec333f05a
Eric Dallo <ericdallo06@hotmail.com>
2021-06-01 23:29:53 -0300

    Renames, fix mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,31 +112,6 @@ $(document).ready(function () {
     ],
   });
 
-  // skill count
-  $(".skill__progress").waypoint(
-    function () {
-      $(".progress-value span").each(function () {
-        $(this)
-          .prop("Counter", 0)
-          .animate(
-            {
-              Counter: $(this).text(),
-            },
-            {
-              duration: 3000,
-              easing: "swing",
-              step: function (now) {
-                $(this).text(Math.ceil(now));
-              },
-            }
-          );
-      });
-      $(".skill__progress_item").addClass("js-animation");
-      this.destroy();
-    },
-    { offset: "80%" }
-  );
-
   // Testimonial slider
   $(".testimonial__slider").slick({
     infinite: true,
